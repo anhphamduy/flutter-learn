a97fcd8f37ba18fcf9801bbeb89244c0c948ab69
Anh Pham <anh.pham@lodlaw.com>
2019-09-12 14:02:35 +1000

    Add navigations

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 
+import '../../models/location.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
+  final int _locationID;
+
+  LocationDetail(this._locationID);
+
   @override
   Widget build(BuildContext context) {
+    final location = Location.fetchById(_locationID);
+
     return Scaffold(
-        appBar: AppBar(title: Text('hello')),
+        appBar: AppBar(title: Text(location.name)),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ImageBanner("assets/images/kiyomizu-dera.jpg"),
-              TextSection(Colors.red),
-              TextSection(Colors.blue),
-              TextSection(Colors.green)
-            ]));
+              ImageBanner(location.imagePath),
+            ]..addAll(textSections(location))));
+  }
+
+  List<Widget> textSections(Location location) {
+    print(location.facts.first.title);
+    print(location.facts.first.text);
+
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList();
   }
 }
